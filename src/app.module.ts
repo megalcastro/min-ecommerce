8034901418170f3c88cb0a9ca804cf513889ec1b
Luis Castro <luisd.ing.pro@gmail.com>
2024-08-12 08:38:05 -0500

    add all depencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from './databases/database.module';
-import { ProductsModule } from './products/products.module';
+import { ProductsModule } from './app/products/products.module';
+import { CustomerModule } from './app/customer/customer.module';
+import { OrderItemModule } from './app/order-item/order-item.module';
+import { OrderModule } from './app/order/order.module';
 
 
 @Module({
-  imports: [DatabaseModule, ProductsModule],
+  imports: [DatabaseModule, ProductsModule, CustomerModule, OrderItemModule, OrderModule],
   controllers: [],
   providers: [],
 })
